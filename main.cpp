cd89ce7f7d12ffb77189f33cdaae8646f2f43e09
pedmands <pedmands@gmail.com>
2020-04-08 16:04:20 -0400

    Final touches

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 // Preston Edmands
 // CSCI 130 - 002
 // Spring 2020
-// Program 4
+// Program 4 - v2
 // This program calculates the cost of throwing a child’s birthday party as determined by the age of the birthday child and the number of guests invited, plus the cost of the birthday cake. It then outputs the report, breaking down the number of each item to purchase and the cost of each item, as well as the total cost, to a file name determined by the user.
+// v2 notes: The program now calculates price breakpoints for balloons, napkins and plates, the and number of full and half sheet cakes needed to feed the party.
 
 #include <iostream>
 #include <iomanip>
@@ -17,7 +18,7 @@ int GetGuests();
 //float GetCakeCost();
 string GetFileName();
 
-float GetBalloonsCost(int numBaloons);
+float GetBalloonsCost(int numBalloons);
 float GetNapkinsCost(int napkinPacks, int NAPKINS_PER_PACK);
 float GetPlatesCost(int platePacks, int PLATES_PER_PACK);
 
@@ -45,7 +46,7 @@ int main() {
     // Welcome the user
     cout << "Welcome to the Birthday Party Cost Calculator" << endl << endl;
     
-    // --------------------- Input ---------------------
+    // -------------------------------- Input --------------------------------
     
     // Ask the user for the child's name
     name = GetName();
@@ -62,7 +63,7 @@ int main() {
     // Get the file name for the output file from the user
     fileName = GetFileName();
     
-    // --------------------- Calculations ---------------------
+    // -------------------------------- Calculations --------------------------------
     
     // Calculate total number of children, including birthday child
     int children = guests + 1;
@@ -95,51 +96,33 @@ int main() {
     float halfSheetCost = 0; // Cost of half sheet of cake
     float fullSheetCost = 0; // Cost of full sheet cakes
     
-    bool halfSheet = false;
+    bool halfSheet = false; // We don't buy a cake until we calculate how many we need
     
-    // Calculate number of full sheets of cake
-    int fullSheets = guests / 50; // Each full sheet feeds 50 guests
-    int remainingGuests = guests % 50; // Remaining guests after dividing by 50
+    int fullSheets = children / 50; // Each full sheet feeds 50 children
+    int remainingGuests = children % 50; // Remaining children after dividing by 50
     
     if (remainingGuests > 0 && remainingGuests <= 25){ // Half sheets only feed 25 people
         halfSheet = true;
-        halfSheetCost = COST_PER_HALF_SHEET;
+        halfSheetCost = COST_PER_HALF_SHEET; // Add cost of half sheet
     } else if (remainingGuests > 25){ // If we have more than 25 people left, we need another full sheet
         fullSheets += 1;
     }
     
-    fullSheetCost = fullSheets * COST_PER_FULL_SHEET;
+    fullSheetCost = fullSheets * COST_PER_FULL_SHEET; // Total cost of full sheet cakes
     
     // Calculate the total cost
+    // We leave halfSheetCost and fullSheetCost as separate variables instead of combining them because they appear as line items in the report
     float total = balloonsCost + giftBagsCost + napkinsCost + platesCost + pizzaCost + juiceCost + halfSheetCost + fullSheetCost;
     
-    // --------------------- Output ---------------------
+    // -------------------------------- Output --------------------------------
     
     // Print the results to the ofstream output file partyReport.out, located in the same directory as the program.
-    PrintReport(fullSheets,
-                halfSheet,
-                fileName,
-                name,
-                numBalloons,
-                balloonsCost,
-                numBags,
-                giftBagsCost,
-                napkinPacks,
-                napkinsCost,
-                platePacks,
-                platesCost,
-                pizzas,
-                pizzaCost,
-                juiceBoxPacks,
-                juiceCost,
-                halfSheetCost,
-                fullSheetCost,
-                total);
+    PrintReport(fullSheets, halfSheet, fileName, name, numBalloons, balloonsCost, numBags, giftBagsCost, napkinPacks, napkinsCost, platePacks, platesCost, pizzas, pizzaCost, juiceBoxPacks, juiceCost, halfSheetCost, fullSheetCost, total);
     
     return 0;
 }
 
-// --------------------- User Input Functions ---------------------
+// ------------------------------- User Input Functions -------------------------------
 
 // GetName() gets the birthday child's name from user and returns it to main()
 // Precondition: none
@@ -194,31 +177,35 @@ string GetFileName() {
     return fileName;
 }
 
-// --------------------- Price breakpoint functions ---------------------
+// ---------------------------- Price breakpoint functions ----------------------------
 
-// GetBalloonsCost
-float GetBalloonsCost(int numBaloons) {
+// GetBalloonsCost() calculates the price of each balloon based on the number of balloons purchased
+// Precondition: numBalloons
+// Postcondition: returns total for balloons
+float GetBalloonsCost(int numBalloons) {
     float costPerBalloon; // Cost per balloon
-    float total;
+    float total; // total cost of balloons
     
-    if (numBaloons > 100){
+    if (numBalloons > 100){
         costPerBalloon = .25;
     }
-    else if (numBaloons >= 50) {
+    else if (numBalloons >= 50) {
         costPerBalloon = .35;
     }
     else {
         costPerBalloon = .45;
     }
-    total = numBaloons * costPerBalloon;
+    total = numBalloons * costPerBalloon;
     return total;
 }
 
-// GetNapkinsCost
+// GetNapkinsCost() calculates the price of each napkin pack based on the total number of napkins purchased
+// Precondition: napkinPacks, NAPKINS_PER_PACK
+// Postcondition: returns total cost of napkins
 float GetNapkinsCost(int napkinPacks, int NAPKINS_PER_PACK) {
     int numNapkins = napkinPacks * NAPKINS_PER_PACK; // Total number of napkins
     float costPerPack; // Cost per napkin pack
-    float total;
+    float total; // total cost of napkins
     
     if (numNapkins >= 300){
         costPerPack = 1.50;
@@ -233,11 +220,13 @@ float GetNapkinsCost(int napkinPacks, int NAPKINS_PER_PACK) {
     return total;
 }
 
-// GetPlatesCost
+// GetPlatesCost() calculates the price of each plate pack based on the total number of plates purchased
+// Precondition: platePacks, PLATES_PER_PACK
+// Postcondition: returns total cost of plates
 float GetPlatesCost(int platePacks, int PLATES_PER_PACK) {
-    int numPlates = platePacks * PLATES_PER_PACK; // Total number of napkins
-    float costPerPack; // Cost per napkin pack
-    float total;
+    int numPlates = platePacks * PLATES_PER_PACK; // Total number of plates
+    float costPerPack; // Cost per plate pack
+    float total; // total cost of plates
     
     if (numPlates > 50){
         costPerPack = 3.00;
@@ -249,7 +238,7 @@ float GetPlatesCost(int platePacks, int PLATES_PER_PACK) {
     return total;
 }
 
-// --------------------- Utility Functions ---------------------
+// -------------------------------- Utility Functions --------------------------------
 
 // PrintReport() prints the report generated by the program to the ofstream output file partyReport.out, located in the same directory as the program.
 // Precondition: fileName, name, numBalloons, balloonsCost, numBags, giftBagsCost, napkinPacks, napkinsCost, platePacks, platesCost, pizzas, pizzaCost, juiceBoxPacks, juiceCost, cakeCost, total
@@ -267,20 +256,20 @@ void PrintReport(int fullSheets, bool halfSheet, string fileName, string name, i
     
     partyReport << endl;
     partyReport << "Report for " << name << "'s birthday party:" << endl << endl;
-    partyReport << "  Balloons:       " << setw(4) << numBalloons << "   $ " << setw(7) << balloonsCost << endl;
-    partyReport << "  Gift Bags:      " << setw(4) << numBags << "   $ " << setw(7) << giftBagsCost << endl;
-    partyReport << "  Napkin Packs:   " << setw(4) << napkinPacks << "   $ " << setw(7) << napkinsCost << endl;
-    partyReport << "  Plate Packs:    " << setw(4) << platePacks << "   $ " << setw(7) << platesCost << endl;
-    partyReport << "  Pizzas:         " << setw(4) << pizzas << "   $ " << setw(7) << pizzaCost << endl;
-    partyReport << "  Juicebox Packs: " << setw(4) << juiceBoxPacks << "   $ " << setw(7) << juiceCost << endl;
+    partyReport << "  Balloons:         " << setw(4) << numBalloons << "   $ " << setw(7) << balloonsCost << endl;
+    partyReport << "  Gift Bags:        " << setw(4) << numBags << "   $ " << setw(7) << giftBagsCost << endl;
+    partyReport << "  Napkin Packs:     " << setw(4) << napkinPacks << "   $ " << setw(7) << napkinsCost << endl;
+    partyReport << "  Plate Packs:      " << setw(4) << platePacks << "   $ " << setw(7) << platesCost << endl;
+    partyReport << "  Pizzas:           " << setw(4) << pizzas << "   $ " << setw(7) << pizzaCost << endl;
+    partyReport << "  Juicebox Packs:   " << setw(4) << juiceBoxPacks << "   $ " << setw(7) << juiceCost << endl;
     if (fullSheets) {
-        partyReport << "  Cake - sheet(s):" << setw(4) << fullSheets << "   $ " << setw(7) << fullSheetCost << endl;
+        partyReport << "  Cake - sheet(s):  " << setw(4) << fullSheets << "   $ " << setw(7) << fullSheetCost << endl;
     }
     if (halfSheet){
-        partyReport << "  Cake - half sheet: " << setw(4) << "1   $  " << halfSheetCost << endl;
+        partyReport << "  Cake - half sheet:   " << setw(4) << "1   $   " << halfSheetCost << endl;
     }
     
-    partyReport << "  Total cost:            $ " << setw(7) << total << endl << endl;
+    partyReport << "  Total cost:              $ " << setw(7) << total << endl << endl;
     partyReport << string(40, '*') << endl;
     
     // Close the report file
